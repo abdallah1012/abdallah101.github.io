d3285ac6372f3908f50ff69a8da58f23921920e0
abdallah101 <abdallah1012@outlook.com>
2021-01-14 21:51:01 +0200

    fixed it

--- JQFunctions.js
+++ JQFunctions.js
@@ -9,20 +9,26 @@ $(document).ready(function(){
       $("#hide").click(function(){
 
         if($("#p1").is(":visible")){
-          $("#p1").slideToggle(function(){
-            $("#p2").css({marginLeft: '300px'}); //use fade toggle to toggle using hiding and show
-          });   
+          $("#p1").slideToggle();   
         }else{
-          $("#p2").css({marginLeft: '0px'});
           $("#p1").slideToggle()
         }
 
       });
-      
-      $("#anim").click(function(){
-        $("#p2").animate({width: '+=200px'}).animate({width: '-=200px'});
-      });
+	  
+	  $("#hide2").click(function(){
 
+        if($("#p2").is(":visible")){
+          $("#p2").slideToggle(function(){
+            
+          });   
+        }else{
+          $("#p2").slideToggle()
+        }
+
+      });
+      
+    
       
 
 
